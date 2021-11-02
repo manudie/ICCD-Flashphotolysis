f60fdbe1e691bb364f8faf1faa1c6263e17f01e0
manudie <manuel.diedrich@yahoo.de>
2021-11-02 15:39:40 +0100

    edited describing information

--- iccd_evaluation.py
+++ iccd_evaluation.py
@@ -2,7 +2,7 @@
 """
 This script was written to evaluate 1024x1024 px images (from the Andor iStar ICCD Camera).
 It allows the user to generate a heatmap and/or a spectrum images from a specified area of lines in the exported sensor data.
-It is essential, to export the data as an .asc (ASCII) file and append measurement information (tick the box in the export window (!)) in the Andor SOLIS Software. 
+It is essential, to export the data as an .asc (ASCII) file and append acquisition information (tick the box in the export window (!)) in the Andor SOLIS Software. 
 The exportet data than just needs to be in the same folder as this evaluation script. It is possible to place multiple files of raw .asc data into to folder, the script will iterate through every single one.
 To decide wether to plot a heatmap, a spectrum or both just create an object from the class iccd_evaluation("String") and change "String" to "heatmap", "spectrum", or "both", e.g. plot1 = iccd_evaluation("both").
 The spectrum is calculated as mean from rows in the image. By changing the values of self.mean_row_start and self.mean_row_end you can determine the span of rows, the  mean is calculated from.
