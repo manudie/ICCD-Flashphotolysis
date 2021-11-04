52ac16854ad0e822f1918b1ed93007ed260c82b9
manudie <manuel.diedrich@yahoo.de>
2021-11-04 11:27:58 +0100

    Added handling for single track readout mode and added test run mode

--- iccd_evaluation.py
+++ iccd_evaluation.py
@@ -27,11 +27,13 @@ class iccd_evaluation():
     def __init__(self, mode):
         ### constructor that declares class variables (self.x) ###
         self.mode = mode
+        self.test_run = True            # If self.test_run is True, Images will not be safed, but jut displayed and also files will not be moved. Made for easier developing.
         self.file = ""
+        self.readout_mode = ""          # Supported: "Full Resolution Image" ("FRI") or "Single Track" ("ST")
         self.single_row = False
         self.input_row = 255
-        self.mean_row_start = 740
-        self.mean_row_end = 760
+        self.mean_row_start = 500
+        self.mean_row_end = 900
         self.peak_height_min = 1500
         self.peak_distance = 20
         self.wavelenght_cal_1 = 404.6565
@@ -41,7 +43,19 @@ class iccd_evaluation():
     
     def read_file(self):
         ### imports the ASCII (.asc) file exportet from the Andor SOLIS software and generates a pandas dataframe from it ###
-        self.ascii_grid = pd.read_table(self.file, engine="python", skipfooter=29, index_col=1, usecols=[*range(1,1024)], header=None) 
+        with open(self.file) as f:
+            if "Single Track" in f.read():
+                self.readout_mode = "ST"
+                self.ascii_grid = pd.read_table(self.file, engine="python", skipfooter=29, index_col=0, header=None).dropna(axis=1, how="all")
+            else:
+                f.seek(0)
+            if self.readout_mode != "ST":
+                if "Full Resolution Image" in f.read():
+                    self.readout_mode = "FRI"
+                    self.ascii_grid = pd.read_table(self.file, engine="python", skipfooter=29, index_col=0, header=None).dropna(axis=1, how="all") #usecols=[*range(1,1024)],
+                else: 
+                    print('Unsupported readout mode! Choose "Full Resolution Image" or "Single Track"')
+        print(self.ascii_grid)
         self.ascii_grid_transposed = self.ascii_grid.T
         self.image_filename = self.file[:len(self.file)-4]
         try:
@@ -84,25 +98,33 @@ class iccd_evaluation():
             labelbottom=False, # labels along the bottom edge are off
             labelleft=False)
         plt.title(label=self.title, pad=-262, loc="left", color="white")
-        if self.mode == "both":
+        if self.mode == "both" and self.readout_mode == "FRI":
             plt.axhspan(self.mean_row_start, self.mean_row_end, color="red", alpha=0.3, lw=0)
-        plt.savefig(self.image_filename + "_heatmap", bbox_inches="tight")
+        if self.test_run == True: 
+            plt.show()
+        else:
+            plt.savefig(self.image_filename + "_heatmap", bbox_inches="tight")
         plt.clf()
 
     def plot_spectrum(self):
         ### plots the generated dataframe to a spectrum with wavelengths on the x-axis from get_calibration() and saves it to the current folder ###
-        if self.single_row == True:
-            spectrum = self.ascii_grid_transposed.iloc[self.input_row]#[self.input_row:self.input_row+1]  # besser: to_numpy() 
-        else: 
-            spectrum = self.ascii_grid_transposed.iloc[self.mean_row_start:self.mean_row_end].mean()
+        if self.readout_mode == "ST":
+            spectrum = self.ascii_grid_transposed.mean()
+        elif self.readout_mode == "FRI":
+            if self.single_row == True:
+                spectrum = self.ascii_grid_transposed.iloc[self.input_row]
+            else: 
+                spectrum = self.ascii_grid_transposed.iloc[self.mean_row_start:self.mean_row_end].mean()
         self.spectrum_list = spectrum.to_numpy()
         self.get_calibration()
         plt.plot(self.wavelengths, self.spectrum_list)
         plt.title(label=self.title, pad=-262, loc="left")
         plt.xlabel("wavelength λ / nm")
         plt.ylabel("counts")
-        #plt.show()
-        plt.savefig(self.image_filename + "_spectrum", bbox_inches="tight")
+        if self.test_run == True: 
+            plt.show()
+        else:
+            plt.savefig(self.image_filename + "_spectrum", bbox_inches="tight")
         plt.clf()
 
     def evaluate_mode(self):
@@ -140,21 +162,22 @@ class iccd_evaluation():
             if entry.path.endswith(".asc") and entry.is_file():
                 self.file = entry.path
                 self.evaluate_mode()
-                shutil.move(self.file, "processed_files")
-                try:
-                    if self.mode == "spectrum":
-                        shutil.move(self.image_filename + "_spectrum.png", "spectra")
-                    elif self.mode == "heatmap":
-                        shutil.move(self.image_filename + "_heatmap.png", "iccd_heatmaps")
-                    elif self.mode == "both":
-                        shutil.move(self.image_filename + "_spectrum.png", "spectra")
-                        shutil.move(self.image_filename + "_heatmap.png", "iccd_heatmaps")
-                except:
-                    #os.remove(self.image_filename + ".png")
-                    print("Failed to safe data!")
-        
+                if self.test_run != True: 
+                    shutil.move(self.file, "processed_files")
+                    try:
+                        if self.mode == "spectrum":
+                            shutil.move(self.image_filename + "_spectrum.png", "spectra")
+                        elif self.mode == "heatmap":
+                            shutil.move(self.image_filename + "_heatmap.png", "iccd_heatmaps")
+                        elif self.mode == "both":
+                            shutil.move(self.image_filename + "_spectrum.png", "spectra")
+                            shutil.move(self.image_filename + "_heatmap.png", "iccd_heatmaps")
+                    except:
+                        #os.remove(self.image_filename + ".png")
+                        print("Failed to safe data!")
+
 
 if __name__ =='__main__':
-    plot1 = iccd_evaluation("both")         # calling an object from the class iccd_evaluation("String") with the parameters "heatmap", "spectrum", or "both"
+    plot1 = iccd_evaluation("spectrum")         # calling an object from the class iccd_evaluation("String") with the parameters "heatmap", "spectrum", or "both"
     plot1.evaluate()                        # start evaluating process by calling the function evaluate()
     
